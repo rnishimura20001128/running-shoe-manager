614239706af3a807d767614a2b5cf34e4b00a95d
ni4mten <r.nishimura20001128@gmail.com>
2026-03-25 01:39:33 +0900

    feat:詳細表示用のモーダル追加とEntityのカラム拡張

--- src/main/java/com/example/running_shoe_manager/Shoe.java
+++ src/main/java/com/example/running_shoe_manager/Shoe.java
@@ -19,4 +19,9 @@ public class Shoe {
     private Double totalDistance = 0.0; // 走行距離
     private LocalDate purchasedAt; // 購入日
     private Boolean isActive = true; // 現役かどうか
+    private String category;       // カテゴリ
+    private Float size;           // サイズ(小数第一位までしか入力しない想定、8バイトのdoubleより小さい4バイトのfloatを採用)
+    private Double limitDistance = 0.0; // 上限走行距離
+    private Boolean hasPlate = false; // カーボンプレート有無
+    private String memo;             // メモ
 }
